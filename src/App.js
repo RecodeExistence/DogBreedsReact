3e21f91815ea5b3a2fd3b7a6114097a3cfd915f8
Recode <recodeexistance@gmail.com>
2021-04-04 22:41:50 +0100

    Added heading component and styles.  Extracted searchbox state update to it's own method.  Did some other styling stuff.  Don't want to spend too long on style now, so this is MVP.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import { Heading } from "./components/heading/heading.component"; 
 import { CardList } from "./components/card-list/card-list.component";
-import { SearchBox } from "./components/searchbox/searchbox.component"
+import { SearchBox } from "./components/searchbox/searchbox.component"; 
+
 class App extends Component {
   constructor() {
     super();
@@ -11,7 +13,6 @@ class App extends Component {
       searchValue: "",
     };
   }
-
   componentDidMount() {
     fetch(
       "https://api.thedogapi.com/v1/breeds?api_key=4d455d0d-1bda-4a33-b886-85528b7acf78"
@@ -19,6 +20,11 @@ class App extends Component {
       .then((response) => response.json())
       .then((dogBreeds) => this.setState({ dogBreeds: dogBreeds }));
   }
+
+  handleSearchValueChange = e => {
+	this.setState({ searchValue : e.target.value});
+   }
+   
   render() {
     const { dogBreeds, searchValue } = this.state;
     const filteredDogs = dogBreeds.filter((dog) =>
@@ -26,11 +32,11 @@ class App extends Component {
     );
     return (
       <div className="App">
+	  <Heading />
 	  <SearchBox 
 	  	placeholder="Search by breed"
-		handleSearchValueChange={e => this.setState({ searchValue : e.target.value}, () => console.log(this.state.searchValue))}
+		handleSearchValueChange={this.handleSearchValueChange}
 	/>
-        
         <CardList dogBreeds={filteredDogs} />
       </div>
     );
